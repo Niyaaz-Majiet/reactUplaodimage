b644b5c02a1405aa861dff779705e366c8e95bae
Niyaaz A.Majiet <43752124+Niyaaz-Majiet@users.noreply.github.com>
2019-08-28 10:14:12 +0200

    Delete firebaseConfig.js
    ...

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default{
-    apiKey: "AIzaSyDsT4vP0DBp4_pOwnhGn6CXi5t4rWS9JQA",
-    authDomain: "upload-img-2a6ad.firebaseapp.com",
-    databaseURL: "https://upload-img-2a6ad.firebaseio.com",
-    projectId: "upload-img-2a6ad",
-    storageBucket: "upload-img-2a6ad.appspot.com",
-    messagingSenderId: "299749787104",
-    appId: "1:299749787104:web:77d7bd59be8b58b6"
-}
